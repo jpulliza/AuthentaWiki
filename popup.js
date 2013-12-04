188743c5ced64d17d63e32c835a8bee4b40dca52
Jonathan Pulliza <jpulliza@gmail.com>
2013-12-03 21:44:42 -0500

    Change popup.js to make the Click Here pop in a new tab.

--- popup.js
+++ popup.js
@@ -1,9 +1,14 @@
+//var markup = document.documentElement.innerHTML;
+//alert(markup)
+
 var tablink
 
 chrome.tabs.getSelected(null, function (tab) {
     var tablink = tab.url;
     var article = tablink.slice(tablink.lastIndexOf("/")+1, tablink.length);
+
     insert = "https://tools.wmflabs.org/xtools/articleinfo/index.php?article=" + article + "&lang=en&wiki=wikipedia"
 
-    document.getElementById("mydiv").innerHTML = '<iframe src=' + insert + ' sandbox="" width="100%" height="100%" frameborder="0"></iframe>';
+    document.getElementById("mydiv").innerHTML = '<p>For the full report click <a href="' + insert + '"target="_blank">HERE.</a></p><iframe src=' + insert + ' sandbox="" width="100%" height="100%" frameborder="0"></iframe>';
+
 });
